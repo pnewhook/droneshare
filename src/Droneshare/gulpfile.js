082b888c06460237e4d3e48a6f3597efb89859fd
Peter Newhook <peter.newhook@gmail.com>
2015-03-19 22:00:22 -0400

    begining asp.net integration

---
+++ src/Droneshare/gulpfile.js
@@ -0,0 +1,16 @@
+﻿"use strict";
+
+var gulp = require('gulp');
+var gulpConcat = require('gulp-concat');
+var bower = require('gulp-bower');
+var mainBowerFiles = require('main-bower-files');
+
+gulp.task('bower', function () {
+    return gulp.src(mainBowerFiles())
+        .pipe(gulp.dest("wwwroot/lib"));
+    
+});
+
+gulp.task('default', function () {
+    
+});
