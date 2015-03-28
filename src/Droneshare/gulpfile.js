a16c4b4cf55c675e0155a66c0bd7356d9f3a00b4
Peter Newhook <peter.newhook@gmail.com>
2015-03-28 09:39:07 -0400

    attempt at gulp

--- src/Droneshare/gulpfile.js
+++ src/Droneshare/gulpfile.js
@@ -4,13 +4,48 @@ var gulp = require('gulp');
 var gulpConcat = require('gulp-concat');
 var bower = require('gulp-bower');
 var mainBowerFiles = require('main-bower-files');
+var gulpDebug = require('gulp-debug');
+
 
 gulp.task('bower', function () {
-    return gulp.src(mainBowerFiles())
-        .pipe(gulp.dest("wwwroot/lib"));
-    
+    return bower();
+})
+
+gulp.task('vendor', ['bower'], function () {
+    return gulp.src(['spin.js/spin.js',
+            'ladda-bootstrap/dist/ladda.js',
+            '../Scripts/custom/atmosphere.js',
+            'jquery/jquery.js',
+            'flot/jquery.flot.js',
+            'flot/jquery.flot.time.js',
+            'ng-file-upload/angular-file-upload-shim.js',
+            'angular/angular.js',
+            '../Scripts/libs/mapbox.js',
+            'ngprogress-lite/ngprogress-lite.js',
+            'angular-animate/angular-animate.min.js',
+            'angular-route/angular-route.js',
+            '../Scripts/custom/angular-leaflet-directive.js',
+            '../Scripts/custom/angular-atmosphere.js',
+            'angulartics/dist/angulartics.js',
+            'angulartics/angulartics-ga.min.js',
+            '..Scripts/libs/ui-bootstrap-tpls-0.11.0.min.js',
+            'angular-flot/angular-flot.js',
+            'angular-social/angular-social.js',
+            'ng-file-upload/angular-file-upload.min.js',
+            'nginfinitescroll/build/ng-infinite-scroll.js',
+            'ng-ladda-bootstrap/ng-ladda-bootstrap.js',
+            'highcharts-ng/dist/highcharts-ng.js'])
+        .pipe(gulpDebug({ title: 'Vendor Debug' }))
+        .pipe(gulpConcat('vendor.js'))
+        .pipe(gulp.dest("wwwroot/js"));
 });
 
-gulp.task('default', function () {
-    
+gulp.task('app', function () {
+    return gulp.src()
+        .pipe(gulpConcat('app.js'))
+        .pipe(gulp.dest('wwwroot/js/'));
+});
+
+gulp.task('default', ['vendor', 'app'], function () {
+
 });
